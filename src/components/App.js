cef00735eb14678e03b264304fed6b4c9c5f44a2
Andriy Kikot <andray838@gmail.com>
2020-11-22 22:07:48 +0300

    added base importers

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import Profile from './Profile';
+import Statistics from './Statistics';
+import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
+
+import user from '../data/user.json';
+import statisticalData from '../data/statistical-data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
+
 
 export default function App() {
-
+    return (
+        1
+        // <Profile
+        //     name={user.name}
+        //     tag={user.tag}
+        //     location={user.location}
+        //     avatar={user.avatar}
+        //     stats={user.stats}
+        // />
+        // <Statistics title="Upload stats" stats={statisticalData} />
+        // <FriendList friends={friends} />
+        // <TransactionHistory items={transactions} />
+    );
 };
--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+
+// function FriendList() {
+//     return (
+//         <li class="item">
+//             <span class="status"></span>
+//             <img class="avatar" src="" alt="" width="48" />
+//             <p class="name"></p>
+//         </li>
+//     );
+// };
+
+// export default FriendList;
