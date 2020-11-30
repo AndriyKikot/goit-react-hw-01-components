5f66c35e96e8bdd475b0a026bcf4048466740672
Andriy Kikot <andray838@gmail.com>
2020-11-30 23:44:33 +0300

    added statistics css

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,12 +4,12 @@ import styles from './Statistics.module.css';
 function Statistics({ title, stats }) {
   return (
     <section className={styles.statistics}>
-      {title && <h2 className="title">{title}</h2>}
+      {title && <h2 className={styles.title}>{title}</h2>}
       <ul className={styles.statList}>
         {stats.map(item => (
           <li className={styles.item} key={item.id}>
-            <span className="label">{item.label}</span>
-            <span className="percentage">{item.percentage}%</span>
+            <span className={styles.label}>{item.label}</span>
+            <span className={styles.percentage}>{item.percentage}%</span>
           </li>
         ))}
       </ul>
